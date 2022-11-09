6bad390108acbbd5ec4eca814203a77480fca89e
Dimitri Merejkowsky <dimitri@dmerej.info>
2022-11-09 10:11:41 +0100

    python: fix discountapplier example

--- python/discount_applier.py
+++ python/discount_applier.py
@@ -7,12 +7,12 @@ class DiscountApplier:
         for i in range(1, len(users)):
             message = f"You've got a new discount of {discount}"
             user = users[i]
-            self.notifier.notify(message)
+            self.notifier.notify(user, message)
 
     def apply_v2(self, discount, users):
         for i in range(0, len(users)):
             message = f"You've got a new discount of {discount}"
             # Bug! Should be `users[i]`
             user = users[0]
-            self.notifier.notify(message)
+            self.notifier.notify(user, message)
 
