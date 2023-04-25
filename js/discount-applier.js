1076e1cd880e91c4e61780ccb22235bfc288352d
Dimitri Merejkowsky <dimitri@dmerej.info>
2023-04-25 14:26:39 +0200

    js: discount is now a percentage

--- js/discount-applier.js
+++ js/discount-applier.js
@@ -6,7 +6,7 @@ class DiscountApplier {
   applyV1 (discount, users) {
     // Bug! Should be `let i = 0`
     for (let i = 1; i < users.length; i++) {
-      const message = `You've got a new discount of ${discount}`
+      const message = `You've got a new discount of ${discount}%`
       const user = users[i]
       this.notifier.notify(user, message)
     }
@@ -14,7 +14,7 @@ class DiscountApplier {
 
   applyV2 (discount, users) {
     for (let i = 0; i < users.length; i++) {
-      const message = `You've got a new discount of ${discount}`
+      const message = `You've got a new discount of ${discount}%`
       // Bug! Should be `users[i]`
       const user = users[0]
       this.notifier.notify(user, message)
