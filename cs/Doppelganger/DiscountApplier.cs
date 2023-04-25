25598796b7f6025430d6caa6351a395075bb5372
Dimitri Merejkowsky <dimitri@dmerej.info>
2023-04-25 14:24:51 +0200

    cs: DiscountApplier: use `int` for the diff, not `double`
    Slightly nicer output when displaying messages

--- cs/Doppelganger/DiscountApplier.cs
+++ cs/Doppelganger/DiscountApplier.cs
@@ -10,22 +10,22 @@ public class DiscountApplier
         _notifier = notifier;
     }
 
-    public void ApplyV1(double discount, List<User> users)
+    public void ApplyV1(int discount, List<User> users)
     {
         // BUG! should be i = 0
         for (int i = 1; i < users.Count; i++)
         {
-            var message = $"You've got a discount of {discount}";
+            var message = $"You've got a discount of {discount}%";
             var user = users[i];
             _notifier.Notify(user, message);
         }
     }
 
-    public void ApplyV2(double discount, List<User> users)
+    public void ApplyV2(int discount, List<User> users)
     {
         for (int i = 0; i < users.Count; i++)
         {
-            var message = $"You've got a discount of {discount}";
+            var message = $"You've got a discount of {discount}%";
             // BUG! should be users[i]
             var user = users[0];
             _notifier.Notify(user, message);
