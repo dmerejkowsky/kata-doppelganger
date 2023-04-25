4e6d7f540f09022cdd2bb17c817c2c47985072ca
Dimitri Merejkowsky <dimitri@dmerej.info>
2023-04-25 14:26:02 +0200

    python: discount is now a percentage

--- python/discount_applier.py
+++ python/discount_applier.py
@@ -5,14 +5,13 @@ class DiscountApplier:
     def apply_v1(self, discount, users):
         # Bug! Should be `range(0, len(users))`
         for i in range(1, len(users)):
-            message = f"You've got a new discount of {discount}"
+            message = f"You've got a new discount of {discount}%"
             user = users[i]
             self.notifier.notify(user, message)
 
     def apply_v2(self, discount, users):
         for i in range(0, len(users)):
-            message = f"You've got a new discount of {discount}"
+            message = f"You've got a new discount of {discount}%"
             # Bug! Should be `users[i]`
             user = users[0]
             self.notifier.notify(user, message)
-
